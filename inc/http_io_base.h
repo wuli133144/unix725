103ebb5ba00f8de1cd528c1a822d2f29d4b3f544
jackwu <1683358846@qq.com>
2017-07-24 23:33:38 +0000

    open_max_fd@0.1,list.h@o.1

--- inc/http_io_base.h
+++ inc/http_io_base.h
@@ -1,25 +1,19 @@
 
 #include "sock.h"
-#include"epoll_inc.h"
+#include "epoll_inc.h"
 
 #define FDSIZE 1000
 #define EPOLLEVENTS 100
-#define MAXSIZE  1024
-
+#define MAXSIZE 1024
 
 static void do_epoll(int listenfd);
-
-
 
 static void handle_events(int epollfd, struct epoll_event *events, int num,
                           int listenfd, char *buf);
 
-
 static void handle_accpet(int epollfd, int listenfd);
 
-
 static void do_read(int epollfd, int fd, char *buf);
-
 
 static void do_write(int epollfd, int fd, char *buf);
 
@@ -30,21 +24,20 @@ static void modify_event(int epollfd, int fd, int state);
 /*@http_delete_event start@*/
 static void delete_event(int epollfd, int fd, int state);
 
-
-static void do_epoll(int listenfd){
+static void do_epoll(int listenfd) {
 
   int epollfd;
-  
+
   struct epoll_event events[EPOLLEVENTS];
   int ret;
   char buf[MAXSIZE];
   memset(buf, 0, MAXSIZE);
-  
+
   epollfd = Epoll_create(FDSIZE);
-  
+
   add_event(epollfd, listenfd, EPOLLIN);
   for (;;) {
-    
+
     ret = Epoll_wait(epollfd, events, EPOLLEVENTS, -1);
     handle_events(epollfd, events, ret, listenfd, buf);
   }
@@ -55,10 +48,10 @@ static void handle_events(int epollfd, struct epoll_event *events, int num,
                           int listenfd, char *buf) {
   int i;
   int fd;
- 
+
   for (i = 0; i < num; i++) {
     fd = events[i].data.fd;
- 
+
     if ((fd == listenfd) && (events[i].events & EPOLLIN))
       handle_accpet(epollfd, listenfd);
     else if (events[i].events & EPOLLIN)
@@ -77,7 +70,6 @@ static void handle_accpet(int epollfd, int listenfd) {
   else {
     printf("accept a new client: %s:%d\n", inet_ntoa(cliaddr.sin_addr),
            cliaddr.sin_port);
-    //添加一个客户描述符和事件
     add_event(epollfd, clifd, EPOLLIN);
   }
 }
@@ -95,7 +87,6 @@ static void do_read(int epollfd, int fd, char *buf) {
     delete_event(epollfd, fd, EPOLLIN);
   } else {
     printf("read message is : %s", buf);
-    //修改描述符对应的事件，由读改为写
     modify_event(epollfd, fd, EPOLLOUT);
   }
 }
@@ -133,44 +124,35 @@ static void modify_event(int epollfd, int fd, int state) {
   Epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &ev);
 }
 
-
-void http_do_epoll(int listenfd)
-  { return do_epoll(listenfd); 
-    
-  }
-
+void http_do_epoll(int listenfd) { return do_epoll(listenfd); }
 
 void http_handle_events(int epollfd, struct epoll_event *events, int num,
-                       int listenfd, char *buf) {
+                        int listenfd, char *buf) {
   return handle_events(epollfd, events, num, listenfd, buf);
-                       }
+}
 
-void  http_handle_accpet(int epollfd, int listenfd) {
+void http_handle_accpet(int epollfd, int listenfd) {
   return handle_accpet(epollfd, listenfd);
 }
 
-
-
 void http_do_read(int epollfd, int fd, char *buf) {
-            return do_read(epollfd,fd,buf);
+  return do_read(epollfd, fd, buf);
 }
 
 void http_do_write(int epollfd, int fd, char *buf) {
-            return do_write(epollfd,fd,buf);
+  return do_write(epollfd, fd, buf);
 }
 
 void http_add_event(int epollfd, int fd, int state) {
-     return add_event(epollfd, fd, state);
+  return add_event(epollfd, fd, state);
 }
-
 
 void http_modify_event(int epollfd, int fd, int state) {
   return modify_event(epollfd, fd, state);
 }
 
-
 /*@http_delete_event start@*/
-void http_delete_event(int epollfd, int fd, int state){
-      return delete_event(epollfd, fd, state);
+void http_delete_event(int epollfd, int fd, int state) {
+  return delete_event(epollfd, fd, state);
 }
 /*@http_delete_event end @*/
--- inc/http_task.h
+++ inc/http_task.h
@@ -1,4 +1,45 @@
 #ifndef __HTTP_TASK_H
-#define 
-#include"http_ipc.h"
+#define __HTTP_TASK_H
 
+#include "http_ipc.h"
+#include "list.h" //========>freebsd kernelcode
+#include "types.h"
+
+
+#include<sys/resource.h>
+
+
+#define NPROC_MAX_NUM        10
+
+#define  MAX_FD_NUM          200         
+
+
+typedef struct __processor__node {
+  i_32 pid;
+  i_32 nleft;
+  SLIST_ENTRY(__processor__node) entry;
+} processor_t;
+
+
+/*@global processor pool object@*/
+   SLIST_HEAD(s_pool_t,__processor__node)proc_pool=SLIST_INIT(&proc_pool);
+
+/*@global processor pool object@*/
+
+int open_max_fd(){
+     struct rlimt rlim;
+     if((getrlimit(RLIMIT_NOFILE,&rlim))<0){
+          err(1,"error:%s","getrlimit");
+          exit(0);
+     }
+     if(rlim.rlim_max>MAX_FD_NUM){
+         rlim.rlim_max=MAX_FD_NUM;
+     }
+     return rlim.rlim_max;
+}
+
+
+
+
+
+#endif
